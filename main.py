217543ad7f1a17ac16b46b6ebea7e8e72e86e572
Isekai Code <109307799+IsekaiCode@users.noreply.github.com>
2023-07-18 15:56:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import argparse
+
 import torch
 
+from neuralnet import NeuralNet, hidden_size, input_size, num_classes
 from number_writer import Writer
-from neuralnet import NeuralNet, input_size, hidden_size, num_classes
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model_path = './model/model.pth'
+
+parser = argparse.ArgumentParser(description='recognize handwritten digits')
+parser.add_argument('-p', '--path', default='model/model.pth',
+                    type=str, help='path to model')
+args = parser.parse_args()
+model_path = args.path
 
 loaded_model = NeuralNet(input_size, hidden_size, num_classes)
 loaded_model.load_state_dict(torch.load(model_path))
